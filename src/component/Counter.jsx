5e6a8fed907415d19a5227a9414f240130f17c60
Vishal Bhuse <vbhuse1997@gmail.com>
2022-05-17 21:59:55 +0530

    first commit

---
+++ src/component/Counter.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from '../App.css';
+
+let Green = {
+  color :"green"
+}
+let Red = {
+    color :"red"
+  }
+
+export const Counter = () => {
+
+    const [count, setCount] = React.useState(0)
+
+    return (
+        <div className={styles.App}>
+
+            <h1 style={count%2==0 ? Green : Red}>Counter : {count}   </h1>
+
+            
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count * 2)}>Double</button>
+            <button onClick={ () => { if(count >0) setCount(count - 1)}} > Decrement</button>
+
+
+        </div>
+    )
+}
